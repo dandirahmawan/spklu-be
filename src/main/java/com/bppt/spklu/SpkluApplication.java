60c2ddcdbb27fb5f99a5d5168de409780d868775
dandi rahmawan <dandirahmawan95@gmail.com>
2020-10-11 15:23:08 +0700

    cors

--- src/main/java/com/bppt/spklu/SpkluApplication.java
+++ src/main/java/com/bppt/spklu/SpkluApplication.java
@@ -2,12 +2,14 @@ package com.bppt.spklu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
+@Configuration
 public class SpkluApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpkluApplication.class, args);
 	}
-
 }
